4430c6d81ad5c38866f7a6342dd3a3d6c83b14d6
rozaimanyusoff <rozaimanyusoff@gmail.com>
2025-03-23 19:03:09 +0800

    update 7a: correction cors

--- src/middlewares/cors.ts
+++ src/middlewares/cors.ts
@@ -1,7 +1,7 @@
 import cors from 'cors';
 
 export default cors({
-  origin: ['http://localhost:5137', 'http://localhost:3000'],
+  origin: ['http://localhost:5173', 'http://localhost:3000'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
