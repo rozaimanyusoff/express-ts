221732583d9de9e6b269f98673e68591b0dc5563
rozaimanyusoff <rozaimanyusoff@gmail.com>
2025-03-27 13:18:24 +0800

    update 12: update ESM modules

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,48 @@ dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
+/* const aj = arkjet({
+  key: process.env.ARKJET_KEY!,
+  characteristics: ["ip.src"],
+  rules: [
+    shield({
+      mode: "LIVE"
+    }),
+    detectBot({
+      mode: "LIVE",
+      allow: ["CATEGORY:SEARCH_ENGINE"]
+    }),
+    fixedWindow({
+      mode: "LIVE",
+      max: 2,
+      window: 60
+    })
+  ]
+}); */
+
+/* app.use((req: Request, res: Response, next: NextFunction) => {
+  aj.protect(req)
+    .then((decision) => {
+      if (decision.isDenied()) {
+        if (decision.reason.isRateLimit()) {
+          res.status(429).json({
+            message: "Too many requests"
+          });
+        } else {
+          res.status(403).json({
+            message: "Access denied"
+          });
+        }
+      } else {
+        next();
+      }
+    })
+    .catch((err) => {
+      next(err);
+    });
+}); */
+
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   logger.info(`Server is running on port ${PORT}`);
