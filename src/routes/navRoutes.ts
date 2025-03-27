689ce5b2dc573e65560aa4d952c7671986e98f52
rozaimanyusoff <rozaimanyusoff@gmail.com>
2025-03-27 16:13:35 +0800

    update 14a: implement /api/nav/access/user

--- src/routes/navRoutes.ts
+++ src/routes/navRoutes.ts
@@ -3,10 +3,11 @@ import { getNavigationPermissionsHandler, trackRoute, toggleStatusHandler, getNa
 import asyncHandler from "../utils/asyncHandler";
 
 const router = Router();
+
+router.get("/access/user", asyncHandler(getNavigationByIds)); // Get navigation by user's access groups. Has body data of accessgroups
 router.get("/access", asyncHandler(getNavigationPermissionsHandler));
 router.put("/track-route", asyncHandler(trackRoute));
 router.get("/navflat", asyncHandler(getNavigationsUnstructured));
-router.get("/:id", asyncHandler(getNavigationByIds));
 router.get("/", asyncHandler(getNavigations));
 router.post("/", asyncHandler(createNavigationHandler));
 router.put("/:id/status", asyncHandler(toggleStatusHandler));
