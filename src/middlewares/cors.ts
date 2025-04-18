31bfdc33617bfc11b338a5dab1c506e293cb7992
rozaimanyusoff <rozaimanyusoff@gmail.com>
2025-04-18 10:16:51 +0800

    update 19b: update cors

--- src/middlewares/cors.ts
+++ src/middlewares/cors.ts
@@ -1,7 +1,7 @@
 import cors from 'cors';
 
 export default cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000', 'https://adms4.ranhilltechnologies.com.my', 'http://100.1.1.129'],
+  origin: ['http://localhost:5173', 'http://localhost:3000', 'https://adms4.ranhilltechnologies.com.my', 'http://100.1.1.129', 'http://localhost:8080'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
