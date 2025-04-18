4de920d3955665540f703dd5b959bddb9cfa65b4
rozaimanyusoff <rozaimanyusoff@gmail.com>
2025-04-18 23:26:21 +0800

    update 19b: add comment on cors

--- src/middlewares/cors.ts
+++ src/middlewares/cors.ts
@@ -1,5 +1,6 @@
 import cors from 'cors';
 
+/* cors options */
 export default cors({
   origin: ['http://localhost:5173', 'http://localhost:3000', 'https://adms4.ranhilltechnologies.com.my', 'http://100.1.1.129', 'http://localhost:8080'],
   credentials: true,
