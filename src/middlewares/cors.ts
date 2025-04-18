23965f27c7f8a69557abdaf2fd906a7ead65ad76
rozaimanyusoff <rozaimanyusoff@gmail.com>
2025-04-19 00:45:00 +0800

    update 19d: update cors options

--- src/middlewares/cors.ts
+++ src/middlewares/cors.ts
@@ -1,9 +1,25 @@
 import cors from 'cors';
 
-/* cors options */
-export default cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000', 'https://adms4.ranhilltechnologies.com.my', 'http://100.1.1.129', 'http://localhost:8080'],
-  credentials: true,
+const allowedOrigins = [
+  'http://localhost:5173',
+  'http://localhost:3000',
+  'https://adms4.ranhilltechnologies.com.my', // Frontend origin
+  'http://100.1.1.129',
+  'http://localhost:8080',
+  'https://serv.ranhilltechnologies.com.my', // Backend origin (if needed)
+];
+
+const corsOptions = {
+  origin: (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true, // Allow cookies/credentials
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-});
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allow custom headers
+};
+
+export default cors(corsOptions);
