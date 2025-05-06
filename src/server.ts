d9a5001f14742386581e34ce34d9e1f7fa15e1a8
rozaimanyusoff <rozaimanyusoff@gmail.com>
2025-05-06 16:06:06 +0800

    update 22b: update server port

--- src/server.ts
+++ src/server.ts
@@ -4,49 +4,7 @@ import logger from './utils/logger.js';
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
-
-/* const aj = arkjet({
-  key: process.env.ARKJET_KEY!,
-  characteristics: ["ip.src"],
-  rules: [
-    shield({
-      mode: "LIVE"
-    }),
-    detectBot({
-      mode: "LIVE",
-      allow: ["CATEGORY:SEARCH_ENGINE"]
-    }),
-    fixedWindow({
-      mode: "LIVE",
-      max: 2,
-      window: 60
-    })
-  ]
-}); */
-
-/* app.use((req: Request, res: Response, next: NextFunction) => {
-  aj.protect(req)
-    .then((decision) => {
-      if (decision.isDenied()) {
-        if (decision.reason.isRateLimit()) {
-          res.status(429).json({
-            message: "Too many requests"
-          });
-        } else {
-          res.status(403).json({
-            message: "Access denied"
-          });
-        }
-      } else {
-        next();
-      }
-    })
-    .catch((err) => {
-      next(err);
-    });
-}); */
-
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
